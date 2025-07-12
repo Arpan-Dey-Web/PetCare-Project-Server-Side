f65063ee030ac669b8912ff4555dc2565bf567a1
Arpan-Dey-Web <arpandey.web@gmail.com>
2025-07-13 00:02:48 +0600

    find pet owner api created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middle wars
 app.use(cors());
@@ -32,9 +32,10 @@ async function run() {
     // database collections
     const petsCollection = database.collection("pets");
     const usersCollection = database.collection("users");
+    const adoptRequestPetsCollection =
+      database.collection("adopt-request-pets");
 
     // user details saved in database
-
     app.post("/register", async (req, res) => {
       const { name, email, image, role } = req.body;
       const result = await usersCollection.insertOne({
@@ -52,16 +53,58 @@ async function run() {
       const result = await petsCollection.insertOne(petDetails);
       res.send(result);
     });
+
     // get pet which are unadopted
     app.get("/available-pets", async (req, res) => {
       const pets = await petsCollection
         .find({ adopted: false })
         .sort({ createdAt: -1 })
         .toArray();
-
       res.send(pets);
     });
 
+    // get single pet via petId
+    app.get("/pet/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await petsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // request for adotion api
+    app.post("/adoption-request", async (req, res) => {
+      const petDetails = req.body;
+      const result = await adoptRequestPetsCollection.insertOne(petDetails);
+      res.send(result);
+    });
+    // get my added pet api
+    // GET /pets?email=user@example.com
+
+   app.get("/pets/:email", async (req, res) => {
+     try {
+       const {email} = req.params
+       console.log(email);
+
+       if (!email) {
+         return res
+           .status(400)
+           .json({ error: "Email query parameter is required." });
+       }
+
+       const pets = await petsCollection
+         .find({ owner: email }) // your stored user email field
+         .sort({ createdAt: -1 }) // newest pets first
+         .toArray();
+
+       res.json(pets);
+     } catch (error) {
+       console.error("Error fetching pets:", error);
+       res.status(500).json({ error: "Internal server error." });
+     }
+   });
+
+    
+    
     
 
     // await client.db("admin").command({ ping: 1 });
